16be7539a027de03c76b2583dcaa7a707df84e47
Jc mac06 <jcruz21+jc@gmail.com>
2022-04-12 01:49:21 -0600

    Fix Add fix index installed kaban

--- resources/js/Pages/Reports/Index.svelte
+++ resources/js/Pages/Reports/Index.svelte
@@ -1,12 +1,13 @@
 <script context="module">
   import Layout, { title } from '@/Shared/Layout.svelte'
-  import Dropdown, {} from '@/Shared/Dropdown.svelte'
   export const layout = Layout
 </script>
 
 <script>
   import {useForm} from "@inertiajs/inertia-svelte";
-  import {character} from "../Reports/Index.svelte";
+  import LoadingButton from '@/Shared/LoadingButton.svelte';
+  import SelectInput from '@/Shared/SelectInput.svelte'
+  import TextInput from '@/Shared/TextInput.svelte'
 
   $title = 'Reports'
 
@@ -21,12 +22,15 @@
     postal_code: organization.postal_code,
   })
 
+  function store() {
+    $form.post(route('users.store').url())
+  }
 </script>
 
 <div>
   <h1 class="mb-8 font-bold text-3xl">Reports</h1>
 
-  <form on:submit|preventDefault={update}>
+  <form on:submit|preventDefault={store}>>
     <div class="p-8 -mr-6 -mb-8 flex flex-wrap">
       <TextInput
         bind:value={$form.name}
@@ -75,14 +79,9 @@
         label="Postal code:" />
     </div>
     <div class="px-8 py-4 bg-gray-50 border-t border-gray-100 flex items-center">
-      {#if !organization.deleted_at}
-        <button class="text-red-600 hover:underline" tabindex="-1" type="button" on:click={destroy}>
-          Delete Organization
-        </button>
-      {/if}
 
       <LoadingButton loading={$form.processing} class="btn-indigo ml-auto" type="submit">
-        Update Organization
+        Update - Create
       </LoadingButton>
     </div>
   </form>
